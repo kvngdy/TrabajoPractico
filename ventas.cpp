1c280d0e25bb02788c463e8a57bd7ba7692f875d
Kevin Antoriano <kevinantorianogodoy@gmail.com>
2025-07-31 02:41:18 -0300

    vendedores funcional, lector lee el archivo .dat, ventas en desarollo

---
+++ ventas.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+struct Sucursal
+{
+    char Nombre[32];
+    int CodigoDeSucursal;
+};
+
+struct Vendedor
+{
+    char Nombre[32];
+    int CodigoDeVendedor;
+    int CodigoDeSucursal;
+};
+
+struct Venta
+{
+    int fecha; //! AAAAMMDD
+    int CodigoDeVendedor;
+    int CodigoDeProducto;
+    int monto; //??dos espacios de deciamal hay que mostrar con coma de dos espacios (dividir en 100)
+};
+bool verificarFecha(int fecha)
+{
+    // verificar
+    return true; // correcto
+}
+bool verificarCodigoVendedor(int codigo)
+{
+    return true;
+}
+
+Venta cargarVenta()
+{
+    Venta Nventa;
+    do
+    {
+        cout << "dame la fecha en AAAAMMDD" << endl;
+        cin >> Nventa.fecha;
+    } while (!verificarFecha(Nventa.fecha));
+
+    do
+    {
+        cout << "dame el codigo de vendedor" << endl;
+        cin >> Nventa.CodigoDeVendedor;
+
+    } while (!verificarCodigoVendedor(Nventa.CodigoDeVendedor));
+
+    cout << "dame el codigo de producto" << endl;
+    cin >> Nventa.CodigoDeProducto;
+    cout << "dame el monto de la Nventa" << endl;
+    do
+    {
+        cin >> Nventa.CodigoDeProducto;
+    } while (Nventa.monto <= 0);
+
+    return Nventa;
+}
+// donando por un anonimo generozo de vendedores
+bool CodigoDeVendedorRepetido(int n_codigo, Vendedor vendedores[], int tamaño)
+{
+    for (int i = 0; i < tamaño; i++)
+    {
+        if (vendedores[i].CodigoDeVendedor == n_codigo)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+void cargarVendedores(Vendedor vendedores[], int cantidadVendedores, int cantidadSucursales)
+{
+    Sucursal sucursales[cantidadSucursales];
+    FILE *archivo = fopen("vendedores.dat", "rb");
+    if (archivo == NULL)
+    {
+        cout << "No se pudo abrir el archivo.";
+    }
+
+    fread(sucursales, sizeof(Sucursal), 3, archivo);
+    fread(vendedores, sizeof(Vendedor), 4, archivo);
+    fclose(archivo);
+
+    cout << "\n Vendedores \n";
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "\n  Vendedor " << i + 1 << ": \n"
+             << vendedores[i].Nombre;
+        cout << " - Codigo: " << vendedores[i].CodigoDeVendedor;
+        cout << " - Sucursal: " << vendedores[i].CodigoDeSucursal;
+    }
+}
+
+int main()
+{
+    int cantidadVendedores = 3;
+    int tamañoVentasMax = 3; //!!debe ser 1000
+    Venta ventasDia[tamañoVentasMax];
+    Vendedor vendedores[cantidadVendedores];
+    cargarVendedores(vendedores, cantidadVendedores, 3);
+
+    int i = 1;
+    int cargarOtraVenta = 1;
+    do
+    {
+        cargarVenta();
+
+        cout << "ingrese un 1 si quiere ingresar otra venta\n";
+        cin >> cargarOtraVenta;
+        i++;
+    } while (i <= 1000 && cargarOtraVenta == 1);
+
+    return 0;
+}
